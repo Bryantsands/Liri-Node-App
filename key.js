710e7b93b85f1516dc1143a00e34126de1cbb03c
bryantsands <bryantsands@bryantsandstekiMacBook-Pro.local>
2016-12-05 23:14:30 -0800

    Initial commit

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'AuwpB2WohoTLNcLFY7jfeqdyE',
+  consumer_secret: 'Yl4EcXFOX0gKfPCJO9LaRISQEheYZGzZzkMSDACzYlAjqQZ1Ba',
+  access_token_key: '805542330765717504-rSL1jcjdyKwI1Op9KyJHl2QiFu3a34P',
+  access_token_secret: 'sejvVi52rTn14cFXz3AJoJpJ4x5ipHKQfulW0bVkq48gU',
+}
---
+++ liri.js
@@ -0,0 +1,99 @@
+var keys = require('./keys.js');
+
+var twitter = require('twitter');
+
+var spotify = require('spotify');
+
+var request = require('request');
+
+var fs = require('fs');
+
+var client = new twitter(keys.twitterKeys);
+
+
+var command = process.argv[2];
+
+if (command == 'my-tweets'){
+	myTweets()
+};
+if (command == 'spotify-this-song'){
+	mySpotify()
+};
+if (command == 'movie-this'){
+	movie()
+};
+
+function myTweets(){
+
+			var params = {screen_name: 'BrySands'};
+				client.get('statuses/user_timeline' , params , function(error , tweets , response){
+					if (!error) {
+						console.log('error' + error)
+					}else{
+						for (var i = 0; i < tweets.length; i++) {
+							console.log('-------')
+							console.log(tweets[i].text);
+							console.log('-------')
+			}
+		}
+	});
+}
+
+function mySpotify(){
+			var name = [];
+			var userFirst = process.argv[2];
+			var userSecond = process.argv[3];
+
+				spotify.search({ type: 'track', query: 'name'}, function(error, response){
+					if ( err ){
+						console.log('Error occurred:' + err);
+						return;
+					}else{
+				console.log('Artist: ' + response.tracks.items[0].artist);
+		        console.log(' ');
+		        console.log('Song name: ' + response.tracks.items[0].name);
+		        console.log(' ')
+		        console.log('Link: ' + response.tracks.items[0].preview_url);
+		        console.log(' ')
+		        console.log('Album: ' + response.tracks.items[0].album);
+		}
+	})
+};
+
+function movie(){
+			var movieName = 'World War Z';
+			
+			var queryURL = 'http://omdbapi.com/?t=' + movieName + '&tomatoes=true' + '&r=json';
+
+request(queryURL, function(error, response, body){
+	if (error){
+		console.log('Error occured:' + error);
+		return;
+		}else (!error && response.statusCode == 200){
+		var result = JSON.parse(body);
+
+		console.log('Title: ' + result.Title);
+        console.log('');
+        console.log('Year: ' + result.Year);
+        console.log(' ');
+        console.log('IMDB Rating: ' + result.imdbRating);
+        console.log(' ');
+        console.log('Country of Production: ' + result.Country);
+        console.log(' ');
+        console.log('Language: ' + result.Language);
+        console.log(' ');
+        console.log('Plot Summary: ' + result.Plot);
+        console.log('');
+        console.log('Actors: ' + result.Actors);
+        console.log(' ');
+        console.log('Rotten Tomatoes Rating: ' + result.tomatoRating);
+        console.log(' ');
+        console.log('Rotten Tomatoes URL: ' + result.tomatoURL);
+        console.log(' ');
+	}
+})
+};
+
+
+
+
